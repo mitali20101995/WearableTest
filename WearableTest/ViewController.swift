9be98f102c29ba343bd69d4039479c2c00e82353
Mitali Ahir <mitaliahir3@gmail.com>
2019-07-13 14:04:52 -0400

    first commit

---
+++ WearableTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WearableTest
+//
+//  Created by Mitali Ahir on 2019-07-13.
+//  Copyright © 2019 Mitali Ahir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
