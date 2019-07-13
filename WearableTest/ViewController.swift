7f782bed1fdb83db081a1cc6aad93b11727182d7
Mitali Ahir <mitaliahir3@gmail.com>
2019-07-13 18:18:36 -0400

    Game logic implemented

--- WearableTest/ViewController.swift
+++ WearableTest/ViewController.swift
@@ -22,26 +22,60 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        givenSequence = makeList(5)
+        setPattern()
+    }
+
+    @IBAction func inpBtn1Clicked(_ sender: Any) {
+        inputSequence.append(1)
+        checkResult()
+    }
+    @IBAction func inpBtn2Clicked(_ sender: Any) {
+        inputSequence.append(2)
+        checkResult()
+    }
+    @IBAction func inpBtn3Clicked(_ sender: Any) {
+        inputSequence.append(3)
+        checkResult()
+    }
+    @IBAction func inpBtn4Clicked(_ sender: Any) {
+        inputSequence.append(4)
+        checkResult()
+    }
+    
+    func checkResult(){
+        print("Given: ", givenSequence)
+        print("Input: ", inputSequence)
+        let inputSeqLength = inputSequence.count
+        let givenSeqLength = givenSequence.count
+        let recentInputIndex = inputSeqLength - 1
+        let recentInput = inputSequence[recentInputIndex]
+        let expectedInput = givenSequence[recentInputIndex]
         
+        if(recentInput != expectedInput){
+            print("Lose")
+            setPattern()
+        }
+        
+        else if(inputSeqLength == givenSeqLength){
+            print("Won")
+            setPattern()
+        }
+    }
+    
+    func setPattern(){
+        givenSequence = makeList(4)
+        inputSequence = []
         imgView1.image = UIImage(named: String(givenSequence[0]));
         imgView2.image = UIImage(named: String(givenSequence[1]));
         imgView3.image = UIImage(named: String(givenSequence[2]));
         imgView4.image = UIImage(named: String(givenSequence[3]));
         
-    }
-
-    @IBAction func inpBtn1Clicked(_ sender: Any) {
-        inputSequence.append(1)
-    }
-    @IBAction func inpBtn2Clicked(_ sender: Any) {
-        inputSequence.append(2)
-    }
-    @IBAction func inpBtn3Clicked(_ sender: Any) {
-        inputSequence.append(3)
-    }
-    @IBAction func inpBtn4Clicked(_ sender: Any) {
-        inputSequence.append(4)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.imgView1.image = nil
+            self.imgView2.image = nil
+            self.imgView3.image = nil
+            self.imgView4.image = nil
+        }
     }
     
     func makeList(_ n: Int) -> [Int] {
